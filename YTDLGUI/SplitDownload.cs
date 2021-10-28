c4914ea0848bcdf2f2f429e66c7b4b3cf378795f
YU-YEN HSU <sabpprook@gmail.com>
2021-10-28 22:32:19 +0800

    Use range-download

---
+++ YTDLGUI/SplitDownload.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YTDLGUI
+{
+    public class SplitDownload
+    {
+        private string URL { get; set; }
+        private string Filename { get; set; }
+        private Stream FileHandler { get; set; }
+        private int Split { get; set; }
+        private long TotalBytes { get; set; }
+        private long CurrentBytes { get; set; }
+        public bool isDone { get; private set; }
+        private object locker = new object();
+
+        public SplitDownload()
+        {
+            ServicePointManager.DefaultConnectionLimit = 200;
+        }
+
+        public void DownloadFileAsync(string url, string filename, int split = 8)
+        {
+            Task.Run(() =>
+            {
+                URL = url;
+                Filename = filename;
+                Split = split;
+                TotalBytes = ContentLength();
+                FileHandler = File.Create(Filename);
+                CreateTask();
+            });
+        }
+
+        public string GetProgress()
+        {
+            return (Convert.ToDouble(CurrentBytes + 1) / Convert.ToDouble(TotalBytes)).ToString("P0");
+        }
+
+        private long ContentLength()
+        {
+            var request = WebRequest.Create(URL) as HttpWebRequest;
+            request.Method = "HEAD";
+            var response = request.GetResponse() as HttpWebResponse;
+            var length = response.StatusCode == HttpStatusCode.OK ? response.ContentLength : 0L;
+            response.Close();
+            return length;
+        }
+
+        private Task[] CreateTask()
+        {
+            var tasks = new List<Task>();
+
+            var range = TotalBytes / Split;
+
+            for (int i = 0; i < Split; i++)
+            {
+                var start = i * range;
+                var end = (i + 1) * range - 1;
+
+                if (i == Split - 1)
+                    end = TotalBytes - 1;
+
+                var block = new Range(start, end);
+
+                tasks.Add(Task.Run(() =>
+                {
+                    DownloadRange(URL, block);
+                }));
+            }
+
+            return tasks.ToArray();
+        }
+
+        private void DownloadRange(string url, Range range)
+        {
+            var req = WebRequest.Create(url) as HttpWebRequest;
+            req.AddRange(range.Start, range.End);
+            req.Method = "GET";
+            req.UserAgent = "Mozilla/5.0";
+
+            var resp = req.GetResponse();
+
+            var length = resp.ContentLength;
+            var stream = resp.GetResponseStream();
+
+            var buffer = new byte[1024];
+            long index = 0;
+            do
+            {
+                var bytes = stream.Read(buffer, 0, buffer.Length);
+                lock (locker)
+                {
+                    FileHandler.Position = range.Start + index;
+                    FileHandler.Write(buffer, 0, bytes);
+                    CurrentBytes += bytes;
+                }
+                index += bytes;
+            } while (index < length);
+
+            if (CurrentBytes == TotalBytes)
+            {
+                isDone = true;
+                FileHandler.Close();
+            }
+        }
+
+        public class Range
+        {
+            public long Start { get; set; }
+            public long End { get; set; }
+            public Range(long start, long end)
+            {
+                Start = start;
+                End = end;
+            }
+        }
+    }
+}
--- YTDLGUI/fmBinary.cs
+++ YTDLGUI/fmBinary.cs
@@ -10,6 +10,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,12 +18,10 @@ namespace YTDLGUI
 {
     public partial class fmBinary : MaterialSkin.Controls.MaterialForm
     {
-        private readonly string youtube_dl_url = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe";
+        private readonly string ytdlp_url = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe";
         private readonly string ffmpeg_url = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip";
         
-        private WebClient wc1 = new WebClient();
-        private WebClient wc2 = new WebClient();
-        private bool is_youtube_dl = false;
+        private bool is_ytdlp = false;
         private bool is_ffmpeg = false;
 
         public fmBinary()
@@ -33,52 +32,32 @@ namespace YTDLGUI
             SkinManager.ColorScheme = new ColorScheme(Primary.Red900, Primary.Red900, Primary.BlueGrey900, Accent.LightBlue200, TextShade.WHITE);
         }
 
-        private void fmBinary_Load(object sender, EventArgs e)
+        private async void fmBinary_Load(object sender, EventArgs e)
         {
             File.Delete("tmp_1");
             File.Delete("tmp_2");
 
-            var update_binary = Properties.Resources.update;
-            var bat_bytes = new byte[0x400];
-            var aria2c_bytes = new byte[update_binary.Length - 0x400];
-            Array.Copy(update_binary, bat_bytes, bat_bytes.Length);
-            Array.Copy(update_binary, 0x400, aria2c_bytes, 0, aria2c_bytes.Length);
+            var sd1 = new SplitDownload();
+            var sd2 = new SplitDownload();
+            sd1.DownloadFileAsync(ytdlp_url, "tmp_1");
+            sd2.DownloadFileAsync(ffmpeg_url, "tmp_2", 16);
 
-            File.WriteAllBytes("update.bat", bat_bytes);
-            File.WriteAllBytes("aria2c.exe", aria2c_bytes);
-
-            var p = Process.Start(new ProcessStartInfo
+            await Task.Run(() =>
             {
-                FileName = "update.bat",
-                UseShellExecute = false
-            });
-
-            p.WaitForExit();
-
-            if (p.ExitCode != 0)
-            {
-                try
+                while (!sd1.isDone || !sd2.isDone)
                 {
-                    File.Delete("tmp_1");
-                    File.Delete("tmp_2");
-                    File.Delete("tmp_1.aria2");
-                    File.Delete("tmp_2.aria2");
-                    File.Delete("yt-dlp.exe");
-                    File.Delete("ffmpeg.exe");
-                    File.Delete("ffprobe.exe");
-                    File.Delete("update.bat");
-                    File.Delete("aria2c.exe");
-                    Environment.Exit(0);
+                    Invoke(new Action(() => label_Prog1.Text = $"[ {sd1.GetProgress()} ]"));
+                    Invoke(new Action(() => label_Prog2.Text = $"[ {sd2.GetProgress()} ]"));
+                    Thread.Sleep(100);
                 }
-                catch { }
-            }
+            });
 
             if (File.Exists("tmp_1"))
             {
                 if (File.Exists("yt-dlp.exe"))
                     File.Delete("yt-dlp.exe");
                 File.Move("tmp_1", "yt-dlp.exe");
-                is_youtube_dl = true;
+                is_ytdlp = true;
             }
             if (File.Exists("tmp_2"))
             {
@@ -111,82 +90,7 @@ namespace YTDLGUI
                 File.Delete("tmp_2");
                 is_ffmpeg = true;
             }
-            if (is_ffmpeg && is_youtube_dl)
-            {
-                File.Delete("aria2c.exe");
-                File.Delete("update.bat");
-                DialogResult = DialogResult.OK;
-            }
-
-            /*wc1.DownloadProgressChanged += Youtubedl_DownloadProgressChanged;
-            wc2.DownloadProgressChanged += FFmpeg_DownloadProgressChanged;
-            wc1.DownloadFileCompleted += Youtubedl_DownloadFileCompleted;
-            wc2.DownloadFileCompleted += FFmpeg_DownloadFileCompleted;
-            wc1.DownloadFileAsync(new Uri(youtube_dl_url), "tmp_1");
-            wc2.DownloadFileAsync(new Uri(ffmpeg_url), "tmp_2");*/
-        }
-
-        private void Youtubedl_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            var prog = e.ProgressPercentage;
-            label_Prog1.Text = $"[ {prog.ToString().PadLeft(3, ' ')} % ]";
-        }
-
-        private void FFmpeg_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            var prog = e.ProgressPercentage;
-            label_Prog2.Text = $"[ {prog.ToString().PadLeft(3, ' ')} % ]";
-        }
-
-        private void Youtubedl_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            if (File.Exists("tmp_1"))
-            {
-                if (File.Exists("yt-dlp.exe"))
-                    File.Delete("yt-dlp.exe");
-                File.Move("tmp_1", "yt-dlp.exe");
-            }
-            is_youtube_dl = true;
-            if (is_ffmpeg)
-            {
-                DialogResult = DialogResult.OK;
-            }
-        }
-
-        private void FFmpeg_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            if (File.Exists("tmp_2"))
-            {
-                if (File.Exists("ffmpeg.exe"))
-                    File.Delete("ffmpeg.exe");
-
-                if (File.Exists("ffprobe.exe"))
-                    File.Delete("ffprobe.exe");
-
-                using (var zipArchive = new ZipArchive(File.OpenRead("tmp_2"), ZipArchiveMode.Read))
-                {
-                    foreach (var entry in zipArchive.Entries)
-                    {
-                        if (entry.Name.Contains("ffmpeg.exe"))
-                        {
-                            using (var fs = new FileStream("ffmpeg.exe", FileMode.Create, FileAccess.Write))
-                            {
-                                entry.Open().CopyTo(fs);
-                            }
-                        }
-                        if (entry.Name.Contains("ffprobe.exe"))
-                        {
-                            using (var fs = new FileStream("ffprobe.exe", FileMode.Create, FileAccess.Write))
-                            {
-                                entry.Open().CopyTo(fs);
-                            }
-                        }
-                    }
-                }
-                File.Delete("tmp_2");
-            }
-            is_ffmpeg = true;
-            if (is_youtube_dl)
+            if (is_ffmpeg && is_ytdlp)
             {
                 DialogResult = DialogResult.OK;
             }
